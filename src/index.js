ef09d864c2c2170ad5aa9fc9f90148271a9f0736
DiepvensBent <71712367+DiepvensBent@users.noreply.github.com>
2023-10-18 17:46:54 +0200

    feat:local delete method
    added local delete method

--- src/index.js
+++ src/index.js
@@ -50,6 +50,21 @@ app.put('/changeItem/:id', (req, res) => {
         res.status(404).json({ error: 'Item not found' });
     }
 });
+//DELETE
+app.delete('/deleteItem/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = items.findIndex((a) => a.id === id);
+  
+    if (index !== -1) {
+      const deletedItem = items.splice(index, 1)[0];
+      res.json(deletedItem);
+    } else {
+      res.status(404).json({ error: 'Item not found' });
+    }
+  });
+
+
+
 
 app.listen(port, (err)=> {
     if(!err){
