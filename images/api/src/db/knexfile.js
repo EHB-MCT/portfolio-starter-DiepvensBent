aa48dca8968ccfa50a09eaff98c6ae618d399ad7
DiepvensBent <71712367+DiepvensBent@users.noreply.github.com>
2023-10-19 16:39:54 +0200

    refactor: replaced mysql to postgres

--- images/api/src/db/knexfile.js
+++ images/api/src/db/knexfile.js
@@ -1,14 +1,9 @@
 // knexfile.js
 module.exports = {
   development: {
-    client: 'mysql2',
-    connection: {
-      host: 'localhost',  // Replace with your mysql2 host
-      port: 3306,         // Replace with your mysql2 port
-      user: 'root',  // Replace with your mysql2 username
-      password: '',  // Replace with your mysql2 password
-      database: 'test',  // Replace with your mysql2 database name
-    },migrations: {
+    client: 'pg',
+    connection: process.env.PG_CONNECTION_STRING,
+    migrations: {
       tableName: 'knex_migrations'
     }
   },
