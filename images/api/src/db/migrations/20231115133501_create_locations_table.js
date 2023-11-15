909b78e6839055d768e4ad207416f66d6cac9d47
DiepvensBent <71712367+DiepvensBent@users.noreply.github.com>
2023-11-15 19:06:15 +0100

    feat: added migration file for location table

---
+++ images/api/src/db/migrations/20231115133501_create_locations_table.js
@@ -0,0 +1,10 @@
+exports.up = function (knex) {
+    return knex.schema.createTable('locations', (table) =>{
+    table.uuid('uuid').unique().primary();
+    table.string('name');
+    });
+};
+  
+exports.down = function (knex) {
+    return knex.schema.dropTable('locations');
+};
