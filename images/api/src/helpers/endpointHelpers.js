935f44ca2f532e55d4e2f5e5037659d60a4e86ad
DiepvensBent <71712367+DiepvensBent@users.noreply.github.com>
2024-01-03 18:22:11 +0100

    feat: added new rules to checkItemName function
    added rules that say: no white spaces in front or after name, an array which contains symbols and numbers that are not allowed in the name input

--- images/api/src/helpers/endpointHelpers.js
+++ images/api/src/helpers/endpointHelpers.js
@@ -2,8 +2,11 @@ function checkItemName(name){
     if(
         name == null 
         || name.length <=1 
-        || typeof(name) != "string" 
-        || name.length > 16){
+        || typeof(name) != "string"
+        || name.trim() !== name // Check for leading or trailing whitespace
+        || name.length > 16
+        || /[!@#$€%^&*(),.?":{}|<>123456789]/.test(name) // Check if name contains symbol
+        ){
     return false
     }
     return true
