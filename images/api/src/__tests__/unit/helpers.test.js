1754a4f8d7ceae3d60593f6ad38a1a5f749de1f0
DiepvensBent <71712367+DiepvensBent@users.noreply.github.com>
2024-01-03 18:22:59 +0100

    feat: added expectations to unit test
    added expectations which tests the new rules of the checkItemName function

--- images/api/src/__tests__/unit/helpers.test.js
+++ images/api/src/__tests__/unit/helpers.test.js
@@ -8,7 +8,17 @@ test("Check Item Name",() => {
     expect(checkItemName("aznsfdijsbqifbdijsqbsihdbfihjsbdhfbyohqvspiudbipfabidvbaouhbvfdoibaihvbdhabhousvfdusvauvdfyhabofuyeazvofuasbvouhdfbvhoua")).toBe(false);
     expect(checkItemName(false)).toBe(false);
     expect(checkItemName(undefined)).toBe(false);
-
     expect(checkItemName("Henk")).toBe(true);
     expect(checkItemName("Anne Marie")).toBe(true);
+    expect(checkItemName("Anne ")).toBe(false);
+    expect(checkItemName("Anne  ")).toBe(false);
+    expect(checkItemName("  Anne")).toBe(false);
+    expect(checkItemName("Anne-Marie")).toBe(true);
+    expect(checkItemName("Anne-Marie ")).toBe(false);
+    expect(checkItemName("&")).toBe(false);
+    expect(checkItemName("Anne&Marie")).toBe(false);
+    expect(checkItemName("Henk:")).toBe(false);
+    expect(checkItemName("123")).toBe(false);
+    expect(checkItemName("Henk3")).toBe(false);
+
 })
